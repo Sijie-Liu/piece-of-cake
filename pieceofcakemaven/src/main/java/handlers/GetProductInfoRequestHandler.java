cedd86eb812a27ded3334916b0d3828f430bec39
Sijie 😈  Liu <liu.sij@husky.neu.edu>
2018-11-28 17:06:01 -0800

    everything is working but ugly

--- pieceofcakemaven/src/main/java/handlers/GetProductInfoRequestHandler.java
+++ pieceofcakemaven/src/main/java/handlers/GetProductInfoRequestHandler.java
@@ -2,12 +2,36 @@ package handlers;
 
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.json.simple.JSONObject;
 
-public class GetProductInfoRequestHandler implements RequestHandler<Object, String> {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
-    public String handleRequest(Object input, Context context) {
-        context.getLogger().log("input: " + input);
-        return "Hello " + input;
+public class GetProductInfoRequestHandler implements RequestStreamHandler {
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        LambdaLogger logger = context.getLogger();
+
+        JSONObject headerJson = new JSONObject();
+        headerJson.put("Access-Control-Allow-Origin", "*");
+
+        JSONObject responseBody = new JSONObject();
+        responseBody.put("message", "Hello Jay Liu");
+
+        JSONObject response = new JSONObject();
+        response.put("statusCode", "201");
+        response.put("headers", headerJson);
+        response.put("body", responseBody.toString());
+
+        logger.log("Response: " + response.toJSONString());
+
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+        writer.write(response.toJSONString());
+        writer.close();
     }
 }
